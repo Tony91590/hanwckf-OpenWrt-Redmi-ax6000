21d16dd847ec9113ad6ba9fc6600c0953ece581a
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2023-07-07 13:39:37 +0200

    Update diy-part2.sh

--- hanwckf/Redmi-AX6000/diy-part2.sh
+++ hanwckf/Redmi-AX6000/diy-part2.sh
@@ -5,7 +5,7 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-# Modify default IP
+# Custom for REDMI AX6000
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i 's/ntp.tencent.com/0.openwrt.pool.ntp.org/g' package/emortal/default-settings/files/99-default-settings-chinese
