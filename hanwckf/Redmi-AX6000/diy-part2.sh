3b2ec07b016c8ceb726c9956e18cbd6f06eea611
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2023-07-07 05:49:01 +0200

    Update diy-part2.sh

--- hanwckf/Redmi-AX6000/diy-part2.sh
+++ hanwckf/Redmi-AX6000/diy-part2.sh
@@ -21,4 +21,5 @@ sed -i 's,https://github.com/immortalwrt/immortalwrt/issues,https://bugs.openwrt
 sed -i 's,https://github.com/immortalwrt/immortalwrt/discussions,https://forum.openwrt.org/,g' include/version.mk
 cp $GITHUB_WORKSPACE/hanwckf/Redmi-AX6000/data/opkg/distfeeds.conf package/base-files/files/etc/
 cp $GITHUB_WORKSPACE/hanwckf/Redmi-AX6000/data/uci-defaults/090-distfeeds.conf package/base-files/files/etc/uci-defaults/
+chmod 0755 package/base-files/files/etc/uci-defaults/090-distfeeds.conf
 cp $GITHUB_WORKSPACE/hanwckf/Redmi-AX6000/data/etc/banner package/base-files/files/etc/
