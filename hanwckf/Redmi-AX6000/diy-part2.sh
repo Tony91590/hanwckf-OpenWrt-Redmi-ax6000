5c76a848a6b7470a6250e2b30aa7895a47685913
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2023-07-14 18:20:13 +0200

    Update diy-part2.sh

--- hanwckf/Redmi-AX6000/diy-part2.sh
+++ hanwckf/Redmi-AX6000/diy-part2.sh
@@ -8,14 +8,14 @@
 # Custom for REDMI AX6000
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
-sed -i 's/ntp.tencent.com/0.openwrt.pool.ntp.org/g' package/emortal/default-settings/files/99-default-settings-chinese
-sed -i 's/ntp1.aliyun.com/1.openwrt.pool.ntp.org/g' package/emortal/default-settings/files/99-default-settings-chinese
-sed -i 's/ntp.ntsc.ac.cn/2.openwrt.pool.ntp.org/g' package/emortal/default-settings/files/99-default-settings-chinese
-sed -i 's/cn.ntp.org.cn/3.openwrt.pool.ntp.org/g' package/emortal/default-settings/files/99-default-settings-chinese
-sed -i 's,downloads.immortalwrt.org,src/gz openwrt_core,g' package/emortal/default-settings/files/99-default-settings-chinese
-sed -i 's,mirrors.vsean.net/openwrt,#src/gz openwrt_core,g' package/emortal/default-settings/files/99-default-settings-chinese
-sed -i 's/CST-8/auto/g' package/emortal/default-settings/files/99-default-settings-chinese
-sed -i 's,Asia/Shanghai,auto,g' package/emortal/default-settings/files/99-default-settings-chinese
+sed -i 's/time1.apple.com/0.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
+sed -i 's/time1.google.com/1.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
+sed -i 's/time.cloudflare.com/2.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
+sed -i 's/pool.ntp.org/3.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
+
+
+
+
 sed -i 's/ImmortalWrt/OpenWrt/g' include/version.mk
 sed -i 's,https://immortalwrt.org/,https://openwrt.org/,g' include/version.mk
 sed -i 's,https://github.com/immortalwrt/immortalwrt/issues,https://bugs.openwrt.org/,g' include/version.mk
