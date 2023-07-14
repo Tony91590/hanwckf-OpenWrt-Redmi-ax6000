b2ae9dca5226295139ba10ebf32283ae752e9c6b
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2023-07-14 18:27:06 +0200

    Update diy-part2.sh

--- hanwckf/Redmi-AX6000/diy-part2.sh
+++ hanwckf/Redmi-AX6000/diy-part2.sh
@@ -12,10 +12,7 @@ sed -i 's/time1.apple.com/0.openwrt.pool.ntp.org/g' package/base-files/files/bin
 sed -i 's/time1.google.com/1.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
 sed -i 's/time.cloudflare.com/2.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
 sed -i 's/pool.ntp.org/3.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
-
-
-
-
+sed -i 's/default-settings-chn/default-settings/g' include/target.mk
 sed -i 's/ImmortalWrt/OpenWrt/g' include/version.mk
 sed -i 's,https://immortalwrt.org/,https://openwrt.org/,g' include/version.mk
 sed -i 's,https://github.com/immortalwrt/immortalwrt/issues,https://bugs.openwrt.org/,g' include/version.mk
